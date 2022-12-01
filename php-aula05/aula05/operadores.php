3e7f8e93418d827d81eb2af1f29f1c0663911d30
Julio Nascimento Sales <d876538@rede.sp>
2022-12-01 17:23:27 -0300

    commit de teste

---
+++ php-aula05/aula05/operadores.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <title>Operacoes Aritmeticas</title>
+</head>
+<body>
+<div>
+    <?php
+      $n1 = 3;
+      $n2 = 2;
+      $m = ($n1 +$n2) /2;
+
+    echo  "A Soma vale ".($n1+$n2);
+    echo  "<br/>A Subtração vale ".($n1-$n2);
+    echo  "<br/>A Multiplicação vale ".($n1*$n2);
+    echo  "<br/>A Divisão vale ".($n1/$n2);
+    echo  "<br/>O Modulo vale ".($n1%$n2);
+    echo  "<br/>A Média vale $m";
+
+    ?>
+</div>
+</body>
+</html>
+ 
